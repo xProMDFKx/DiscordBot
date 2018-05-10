aedd46a559d08655371ea82283c637a4a3d5d0a4
xProMDFKx <37838961+xProMDFKx@users.noreply.github.com>
2018-05-10 17:59:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -11,14 +11,6 @@ const config = require("./config.json");
 // config.token contains the bot's token
 // config.prefix contains the message prefix.
 
-client.on("ready", () => {
-  // This event will run if the bot starts, and logs in, successfully.
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  // Example of changing the bot's playing game to something useful. `client.user` is what the
-  // docs refer to as the "ClientUser".
-  client.user.setGame('DiscordBot | d!help', 'https://www.twitch.tv/streamerhouse', 'DiscordBot | d!help')
-});
-
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
@@ -285,7 +277,7 @@ client.on('guildMemberRemove', member => {
 });
 
 client.on('ready', () => {
-  client.user.setGame('DiscordBot | d!help', 'https://www.twitch.tv/streamerhouse', 'ShieldZone | sh!help')
+  client.user.setGame('DiscordBot | d!help', 'https://www.twitch.tv/streamerhouse', 'DiscordBot | d!help')
 })
 
 client.on('message', message => {

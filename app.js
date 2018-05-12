c8997a341b41917bdee48182de17cae2dc713e8b
xProMDFKx <37838961+xProMDFKx@users.noreply.github.com>
2018-05-12 08:16:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -54,6 +54,67 @@ client.on("message", async message => {
     m.edit(`✰ | Pong! Latența ta este de ${m.createdTimestamp - message.createdTimestamp}ms. Latența ta API este de ${Math.round(client.ping)}ms`);
   }
   
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function SolveRecaptchaV2(APIKey, googleKey, pageUrl, proxy, proxyType){
+            var requestUrl = "https://2captcha.com/in.php?key=" + APIKey + "&method=userrecaptcha&googlekey=" + googleKey + "&pageurl=" + pageUrl + "&proxy=" + proxy + "&proxytype=";
+
+            switch (proxyType) {
+                case 'HTTP':
+                requestUrl = requestUrl + "HTTP";
+                break;
+
+                case 'HTTPS':
+                requestUrl = requestUrl + "HTTPS";
+                break;
+
+                case 'SOCKS4':
+                requestUrl = requestUrl + "SOCKS4";
+                break;
+
+                case 'SOCKS5':
+                requestUrl = requestUrl + "SOCKS5";
+                break;
+            }   
+            $.ajax({url: requestUrl, success: function(result){
+                if(result.length < 3){
+                    return false;
+                }else{
+                    if(result.substring(0, 3) == "OK|"){
+                        var captchaID = result.substring(3);
+
+                        for(var i=0; i<24; i++){
+                            var ansUrl = "https://2captcha.com/res.php?key=" + APIKey + "&action=get&id=" + captchaID;  
+
+                            $.ajax({url: ansUrl, success: function(ansresult){
+                                    console.log(ansresult);
+                                    if(ansresult.length < 3){
+                                        return ansresult;
+                                    }else{
+                                        if(ansresult.substring(0, 3) == "OK|"){
+                                            return ansresult;
+                                        }else if (ansresult != "CAPCHA_NOT_READY"){
+                                            return ansresult;
+                                        }
+                                    }
+                                }
+                            });
+                            await sleep(1000);
+                        }
+
+                    }else{
+                        return ansresult;   
+                    }
+                }
+            },
+            fail: function(){
+                return "";
+                }
+            });
+
+        }
+  
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 fs.readdir('./commands/', (err, files) => {

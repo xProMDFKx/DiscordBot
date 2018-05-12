b985aae49eac48e965fef7039b914b4250dea564
xProMDFKx <37838961+xProMDFKx@users.noreply.github.com>
2018-05-12 08:44:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -50,19 +50,6 @@ client.on("message", async message => {
     const m = await message.channel.send("Ping?");
     m.edit(`✰ | Pong! Latența ta este de ${m.createdTimestamp - message.createdTimestamp}ms. Latența ta API este de ${Math.round(client.ping)}ms`);
   }
-  
- if(command === "BALANCE" || command === "MONEY") { // This will run if the message is either ~BALANCE or ~MONEY
-
-        // Additional Tip: If you want to make the values guild-unique, simply add + message.guild.id whenever you request.
-        economy.fetchBalance(message.author.id + message.guild.id).then((i) => { // economy.fetchBalance grabs the userID, finds it, and puts the data with it into i.
-            // Lets use an embed for This
-            const embed = new Discord.RichEmbed()
-                .setDescription(`**${message.guild.name} Bank**`)
-                .setColor(0xD4AF37) // You can set any HEX color if you put 0x before it.
-                .addField('Account Holder',message.author.username,true) // The TRUE makes the embed inline. Account Holder is the title, and message.author is the value
-                .addField('Account Balance',i.money,true)
-            message.channel.send({embed})
-        });
                                                                         
   if(command === "say") {
     // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
